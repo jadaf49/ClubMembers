2a44820621a504a3b391c25f7ed0ce4b4a933c09
jadaf49 <jadaf49@nycstudents.net>
2025-05-05 21:44:49 +0000

    first commit

---
+++ ClubMembers.java
@@ -0,0 +1,11 @@
+public class ClubMembers
+{
+    private ArrayList<MemberInfo> memberList;
+    
+    public void addMembers(String[] names, int gradYear)
+    { /* to be implemented in part (a) */ }
+    
+    public ArrayList<MemberInfo> removeMembers(int year)
+    { /* to be implemented in part (b) */ }
+    
+}
---
+++ MemberInfo.java
@@ -0,0 +1,11 @@
+public class MemberInfo
+{
+    public MemberInfo(String name, int gradYear, boolean hasGoodStanding)
+    { /* implementation not shown */ }
+    
+    public int getGradYear()
+    { /* implementation not shown */ }
+
+    public boolean inGoodStanding()
+    { /* implementation not shown */ }
+}
